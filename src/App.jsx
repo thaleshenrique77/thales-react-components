6f77a6f9916df7550a58c0e7be79b35279ef9806
Thales Henrique <thales_csgo@hotmail.com>
2024-03-06 15:49:32 -0300

    primeiro componente em react

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import TecnologiaComponent from '../src/components/TecnologiaComponent';
+
+const App = () => {
+  return (
+    <div>
+      <TecnologiaComponent tecnologia="React" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TecnologiaComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TecnologiaComponent = (props) => {
+  return (
+    <div>
+      <h2 style={{padding: 450}}>Estou estudando: {props.tecnologia}</h2>
+    </div>
+  );
+}
+
+export default TecnologiaComponent
